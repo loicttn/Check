0ebef324cffcc5ad343bc15466625f1c45a7b4d3
TITREN Loïc <loicttn@localhost.localdomain>
2019-06-03 15:55:37 +0200

    Added NULL handling in assert

--- src/assert/asserts.c
+++ src/assert/asserts.c
@@ -3,18 +3,22 @@
  *  Create Time: 2019-06-03 11:58:27
  *  :------------:
  *  Modified by: TITREN Loïc
- *  Modified time: 2019-06-03 13:24:19
+ *  Modified time: 2019-06-03 15:51:40
  *  Description:
  */
 
 #include "Check_asserts.h"
 #include "Check_tests_creator.h"
 
-void assert_eq_ptr(void *str1, void *str2)
+void ck_assert_eq_ptr(void *str1, void *str2)
 {
     char *strA = (char *)str1;
     char *strB = (char *)str2;
 
+    if ((str1 && !str2) || (!str1 && str2))
+        exit(TEST_KO);
+    if (!str1 && !str2)
+        exit(TEST_OK);
     while (*strA && *strB && *strA == *strB) {
         strA += 1;
         strB += 1;
